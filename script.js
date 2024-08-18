0a795d1c7cbbe93d50fc82e1d1c194436c30b749
huzaifhakim <168334400+huzaifhakim@users.noreply.github.com>
2024-08-18 22:29:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function showCars() {
+    document.getElementById('cars-section').style.display = 'block';
+}
+
+function rentCar(carModel) {
+    alert(`You have chosen to rent: ${carModel}`);
+   }
